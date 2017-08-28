35e8e299c26edfcb08ec4ef252d46a5ff2a4107d
andip71 <andreasp@gmx.de>
2017-08-28 14:57:18 +0200

    boeffla_wl_blocker: add generic wakelock blocker driver v1.0.0
    Based on ideas of FranciscoFranco's non-generic driver.
    
    Sysfs node:
    
    	/sys/class/misc/boeffla_wakelock_blocker/wakelock_blocker
    
    		- list of wakelocks to be blocked, separated by semicolons
    
    	/sys/class/misc/boeffla_wakelock_blocker/debug
    
    		- write: 0/1 to switch off and on debug logging into dmesg
    		- read:  get current driver internals
    
    	/sys/class/misc/boeffla_wakelock_blocker/version
    
    		- show driver version
    
    Signed-off-by: andip71 <andreasp@gmx.de>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    (cherry picked from commit f4f911bea2e5ec5381f535bcc46f9311bc695956)
    (cherry picked from commit 66a80a25c663fc0e5157e8fd01e495dfb312009d)
    (cherry picked from commit c35f879f09d30eec7da295fed90024ffb22baa5e)
    (cherry picked from commit c0a0e6ee89ba261c7101155a9ffb9ad125faf6b4)
    (cherry picked from commit c6b4cfc6567e6a47d830deb13c79dacf40327795)
    (cherry picked from commit 21714df16137017af20f07e0b2d95000f52f28f4)
    (cherry picked from commit ce165ab469865eb0aa8fe3e3f4d3e4096e586bad)

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -24,6 +24,14 @@
 #include <linux/irqdesc.h>
 
 #include "power.h"
+
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER
+char list_wl[255];
+char list_wl_search[257];
+bool wl_blocker_active = false;
+bool wl_blocker_debug = false;
+#endif
+
 //[PM_debug +++]
 #include <linux/pm_debug.h>
 //Extern: this flag to check dpm_suspend has been callback for resume_console
@@ -612,6 +620,42 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 	trace_wakeup_source_activate(ws->name, cec);
 }
 
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER
+// AP: Function to check if a wakelock is on the wakelock blocker list
+static bool check_for_block(struct wakeup_source *ws)
+{
+	char wakelock_name[52];
+
+	// if debug mode on, print every wakelock requested
+	if (wl_blocker_debug)
+		printk("Boeffla WL blocker: %s requested\n", ws->name);
+
+	// if there is no list of wakelocks to be blocked, exit without futher checking
+	if (!wl_blocker_active)
+		return false;
+
+	// check if wakelock is in wake lock list to be blocked
+	if (ws)
+	{
+		// wake lock names which are longer than 50 chars are not handled
+		if (strlen(ws->name) > 50)
+			return false;
+
+		sprintf(wakelock_name, ";%s;", ws->name);
+
+		if(strstr(list_wl_search, wakelock_name) == NULL)
+			return false;
+	}
+
+	// wake lock is in list, print it if debug mode on
+	if (wl_blocker_debug)
+		printk("Boeffla WL blocker: %s blocked\n", ws->name);
+
+	// finally block it
+	return true;
+}
+#endif
+
 /**
  * wakeup_source_report_event - Report wakeup event using the given source.
  * @ws: Wakeup source to report the event for.
@@ -619,6 +663,10 @@ static void wakeup_source_activate(struct wakeup_source *ws)
  */
 static void wakeup_source_report_event(struct wakeup_source *ws, bool hard)
 {
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER
+	if (!check_for_block(ws))	// AP: check if wakelock is on wakelock blocker list
+	{
+#endif
 	ws->event_count++;
 	/* This is racy, but the counter is approximate anyway. */
 	if (events_check_enabled)
@@ -626,6 +674,10 @@ static void wakeup_source_report_event(struct wakeup_source *ws, bool hard)
 
 	if (!ws->active)
 		wakeup_source_activate(ws);
+
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER
+	}
+#endif
 
 	if (hard)
 		pm_system_wakeup();
@@ -920,7 +972,10 @@ void pm_print_active_wakeup_sources(void)
 			//pm_pr_dbg("active wakeup source: %s\n", ws->name);
             pm_printk("active wakeup source: %s\n", ws->name);
             //[PM_debug ---]
-			active = 1;
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER
+			if (!check_for_block(ws))	// AP: check if wakelock is on wakelock blocker list
+#endif
+				active = 1;
 		} else if (!active &&
 			   (!last_activity_ws ||
 			    ktime_to_ns(ws->last_time) >
