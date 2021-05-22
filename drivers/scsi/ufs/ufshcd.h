eb0a490d0f84ee0d229088007139296faef954b5
freak07 <michalechner92@googlemail.com>
2021-05-22 10:14:08 +0200

    Revert "scsi: ufs: update UFS initialization sequence"
    This reverts commit d05664f31fe823f5ec27605f839d9dfef0b9a51f.
    
    (cherry picked from commit 958efec1b4bc6f977712b619bf5dc3d2593b90a7)

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -389,6 +389,7 @@ struct ufs_hba_variant_ops {
 	void	(*config_scaling_param)(struct ufs_hba *hba,
 					struct devfreq_dev_profile *profile,
 					void *data);
+
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
@@ -1013,7 +1014,6 @@ struct ufs_hba {
 	u32 limit_rx_hs_gear;
 	u32 limit_tx_pwm_gear;
 	u32 limit_rx_pwm_gear;
-	u32 limit_phy_submode;
 	bool restore_needed;
 	u32 scsi_cmd_timeout;
 	bool auto_h8_err;
