c4e5b7cd3ee0504c489a198ce3505f62ee569815
freak07 <michalechner92@googlemail.com>
2021-05-22 10:13:51 +0200

    Revert "ANDROID: scsi: ufs: Handle double release of clocks"
    This reverts commit d189e4ea6d657f5a19dcdc7b21ff3afc3013970f.
    
    (cherry picked from commit 6bc4eb43aa9878dcacc7050c67820b95b7357246)

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -2661,6 +2661,7 @@ static int ufshcd_queuecommand(struct Scsi_Host *host, struct scsi_cmnd *cmd)
 		ufshcd_release(hba);
 		lrbp->cmd = NULL;
 		clear_bit_unlock(tag, &hba->lrb_in_use);
+		ufshcd_release(hba);
 		goto out;
 	}
 	lrbp->req_abort_skip = false;
